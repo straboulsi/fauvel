5383652e45c4746b277e58738cee905550dd31b0
Alison Y. Chang <alisonychang6@gmail.com>
2013-07-02 11:50:43 -0400

    Changed return of makeBoxes to List<TranslationBox>. Added comments.

--- Fauvel1/Fauvel1/Class1.cs
+++ Fauvel1/Fauvel1/Class1.cs
@@ -17,14 +17,23 @@ using System.Windows;
 
 namespace Fauvel1
 {
+
+    /**
+     * This class fetches information from the various XML files.
+     * It can search for a section of poetry, music lyrics, or image description by tag id.
+     * It can also return specific lines of poetry by line number, both in Old French and in English.
+     * This class also makes an ArrayList of TranslationBoxes when given a page of Fauvel.
+     **/
     static class Class1
     {
 
-        /// Takes in start and end line numbers from the .xaml.cs 
-        /// This is good bc Surface could send the int values of the first/last lines highlighted by user
-        /// The overlay is then flexible and does not limit to translating entire sections of poetry
-        /// <param name="firstLine">First line of target poetry section</param>
-        /// <param name="lastLine">Last line of target poetry section</param>
+        /**Takes in start and end line numbers.
+         * Returns String of Old French poetry.
+         * This is good bc Surface could send the int values of the first/last lines highlighted by user
+         * The overlay is then flexible and does not limit to translating entire sections of poetry
+         * <param name="firstLine">First line of target poetry section</param>
+         * <param name="lastLine">Last line of target poetry section</param>
+        **/
         public static String getPoetry(int firstLine, int lastLine)
         {
 
@@ -39,10 +48,8 @@ namespace Fauvel1
                 
                 for (int i = firstLine; i <= lastLine; i++)
                 {
-
                     foundNode = xml.DocumentElement.SelectSingleNode("//lg/l[@n='"+i+"']");
                     toDisplay += foundNode.InnerText + "\r\n";
-                    ///Console.Write(foundNode.InnerText+"\r\n");
                 }
 
 
@@ -56,10 +63,16 @@ namespace Fauvel1
             return toDisplay;
         }
 
-        public static ArrayList makeBoxes(String page)
+
+        /**
+         * Creates an ArrayList of TranslationBox objects when given a folio page.
+         * Consults the Content (Old French), Layout, and English XML files. 
+         * Calls on other methods in this class to fetch English, French, or coordinates.
+         **/
+        public static List<TranslationBox> makeBoxes(String page)
         {
 
-            ArrayList boxes = new ArrayList();
+            List<TranslationBox> boxes = new List<TranslationBox>();
 
             try
             {
@@ -67,12 +80,9 @@ namespace Fauvel1
                 xml.Load("XMLFinalContentFile.xml");
 
                 XmlNodeList foundNode;
-                
 
                 page = page.Substring(2);
                 foundNode = xml.DocumentElement.SelectNodes("//pb[@facs='#" + page + "']/lg");
-
-                int i = 0;
 
                 foreach (XmlNode x in foundNode)
                 {
@@ -85,16 +95,12 @@ namespace Fauvel1
                     int end = Convert.ToInt32(s.Substring(mid + 1));
                     
                     boxes.Add(new TranslationBox(s, getPoetry(start, end), getEnglish(start,end), getPoint(s,1), getPoint(s,2)));
-                    
-                    i++;
-                }  
+                }
 
-                /**
                 foreach (TranslationBox tb in boxes)
                 {
-                    Console.Write(tb.tag+"\r\n"+tb.pointsToString()+"\r\n");
-                }  
-                **/
+                    Console.Write(tb.tag);
+                }
 
                 Console.Read();
 
@@ -104,17 +110,18 @@ namespace Fauvel1
                 Console.Write(e.StackTrace);
                 Console.Read();
             }
-             
             return boxes;
         }
 
+
+
         /**
-         * The int whichPt = 1 if you want top left point and 2 if you want bottom right
+         * Fetches top left and bottom right coordinates from Layout XML file when given tag id of object.
+         * The int whichPt should = 1 if you want top left point and 2 if you want bottom right.
          **/
         public static Point getPoint(String tag, int whichPt)
         {
             Point TL = new Point();
-            
 
             try
             {
@@ -132,18 +139,18 @@ namespace Fauvel1
                             TL = new Point(Convert.ToInt32(xn.Attributes["lrx"].Value), Convert.ToInt32(xn.Attributes["lry"].Value));
                     }
                 }
-                
             }
             catch (Exception e)
             {
                 Console.Write(e.StackTrace);
                 Console.Read();
             }
-
             return TL;
         }
 
-
+        /**
+         * Fetches English translation for a section of poetry, given starting and ending line numbers.
+         **/
         public static String getEnglish(int start, int end)
         {
             String toDisplay = "";
@@ -154,16 +161,11 @@ namespace Fauvel1
                 xml.Load("EnglishXML.xml");
 
                 XmlNode foundNode;
-
                 for (int i = start; i <= end; i++)
                 {
-
                     foundNode = xml.DocumentElement.SelectSingleNode("//lg/l[@n='" + i + "']");
                     toDisplay += foundNode.InnerText + "\r\n";
-                    ///Console.Write(foundNode.InnerText+"\r\n");
                 }
-
-                ///Console.Read();
             }
             catch (Exception e)
             {
@@ -174,14 +176,16 @@ namespace Fauvel1
             return toDisplay;
         }
 
-
-        /// <summary>
-        /// This method searches for an id value for images, music, and poetry sections
-        /// <figure id="1rIm2"> Searching for image
-        /// <p id="1rMo2"> Searching for music
-        /// <lg> searching by Te_xxxx-xxxx
-        /// </summary>
-        /// <param name="str">The value of the id</param>
+        /**
+         *  <summary>
+         *  This method searches for an id value for images, music, and poetry sections
+         *  <figure id="1rIm2"> Searching for image
+         *  <p id="1rMo2"> Searching for music
+         *  <lg> searching by Te_xxxx-xxxx
+         *  </summary>
+         *  Searching Fo1v or some other page gives you all contents of that page.
+         *  <param name="str">The value of the id</param>
+        **/
         public static String go(String str)
         {
             ///Console.Write("Input: "+ str+"\r\n");
@@ -200,9 +204,7 @@ namespace Fauvel1
                     foundNode = xml.DocumentElement.SelectSingleNode("//lg[@id='"+str+"']");
                     XmlNodeList lineByLine = foundNode.SelectNodes("l");
                     foreach (XmlNode x in lineByLine)
-                    {
                         str += x.InnerText + "\r\n";
-                    }
                 }
                 else if (str.StartsWith("Fo"))
                 {
@@ -232,6 +234,7 @@ namespace Fauvel1
 
         /// <summary>
         ///  [@nv='"+voiceNum+"']
+        ///  Returns a list of the music objects with a certain # of voices
         /// </summary>
         /// <param name="voiceNum"></param>
         public static void filterByVoice(int voiceNum)
